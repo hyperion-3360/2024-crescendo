206aed6d48788cc02edc22f20164878141c3675b
Nikola Zelovic <nikola1998@hotmail.fr>
2024-01-11 19:36:08 -0500

    Indentation and comments

--- src/main/java/frc/robot/subsystems/Gyro.java
+++ src/main/java/frc/robot/subsystems/Gyro.java
@@ -1,7 +1,3 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.subsystems;
 import com.ctre.phoenix.sensors.PigeonIMU;
 import com.ctre.phoenix.sensors.WPI_PigeonIMU;
@@ -10,47 +6,48 @@ import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Gyro extends SubsystemBase {
-  
-  //make a new pigeon
-WPI_PigeonIMU m_gyro = new WPI_PigeonIMU(0);
- 
-//dislplay the yaw pitch and roll on the ShuffleBoard
-GenericEntry m_gyroDataEntry = Shuffleboard.getTab("gyro info").add("gyro Yaw Pitch and Roll data",
-new double[]{0.0,0.0,0.0}).getEntry();
+    // Make a new pigeon
+    WPI_PigeonIMU m_gyro = new WPI_PigeonIMU(0);
 
-//class constuctor
-public Gyro(){
-               //gets the general status VI
-              PigeonIMU.GeneralStatus genStatus = new PigeonIMU.GeneralStatus();
-              m_gyro.getGeneralStatus(genStatus);
+    // Display the yaw pitch and roll on the ShuffleBoard
+    GenericEntry m_gyroDataEntry = Shuffleboard.getTab("gyro info")
+                                                .add("gyro Yaw Pitch and Roll data", new double[]{0.0,0.0,0.0})
+                                                .getEntry();
 
-              //sets the yaw,Pitch,Roll and the fuseHeading
-              m_gyro.setYaw(0);
-              m_gyro.setFusedHeading(0);
-}
+    //class constuctor
+    public Gyro(){
+        // Gets the general status VI
+        PigeonIMU.GeneralStatus genStatus = new PigeonIMU.GeneralStatus();
+        m_gyro.getGeneralStatus(genStatus);
 
- public double[] getYawPitchRoll() {
-     double [] yawPitchRoll = new double[3];
-      m_gyro.getYawPitchRoll(yawPitchRoll);
-      return yawPitchRoll;
-  }
+        // Tare the sensors
+        // Sets the yaw,Pitch,Roll and the fuseHeading
+        m_gyro.setYaw(0);
+        m_gyro.setFusedHeading(0);
+    }
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  m_gyroDataEntry.setDoubleArray(getYawPitchRoll());
-  
-   
-  }
+    public double[] getYawPitchRoll() {
+        double [] yawPitchRoll = new double[3];
+        m_gyro.getYawPitchRoll(yawPitchRoll);
+        return yawPitchRoll;
+    }
 
-  @Override
-  public void simulationPeriodic() {
-    // This method will be called once per scheduler run during simulation
- 
-  }
+    @Override
+    public void periodic() {
+        // This method will be called once per scheduler run
 
-  public void robotInit() {
- }
+        // Send the gyro data to shuffleboard
+        m_gyroDataEntry.setDoubleArray(getYawPitchRoll());
+    }
+
+    @Override
+    public void simulationPeriodic() {
+        // This method will be called once per scheduler run during simulation
+    }
+
+    public void robotInit() {
+        // ...
+    }
 }
 
 
