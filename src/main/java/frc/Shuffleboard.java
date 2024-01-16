d88099be3f98a66d9bac11fa217f115f5b00bf07
Nikola Zelovic <nikola1998@hotmail.fr>
2024-01-16 17:40:36 -0500

    Shuffleboard manager class template

---
+++ src/main/java/frc/Shuffleboard.java
@@ -0,0 +1,28 @@
+package frc;
+
+public class Shuffleboard {
+
+    public Shuffleboard(){
+        // TODO
+    }
+
+    public void addTab(String title){
+        // TODO
+    }
+
+    public void addWidget(String tabId){
+        
+    }
+
+    public void displayData(String title, String widgetId, int[] data){
+        // TODO widgetID ????
+    }
+
+    public void displayData(String title, String widgetId, double[] data){
+        // TODO widgetID ????
+    }
+
+    public void displayData(String title, String widgetId, String[] data){
+        // TODO widgetID ????
+    }
+}
