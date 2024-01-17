c69f1b092904cadf26ded8d91ff0b6b9a3d86f8b
Igor-Volkov3360 <igorvolkovtravail@gmail.com>
2024-01-17 16:40:18 -0500

    added a conversion class

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -1,73 +1,49 @@
 package frc.robot.subsystems;
 
 import static frc.robot.Constants.WCPSwerveModule.kLocations;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
-import edu.wpi.first.wpilibj.smartdashboard.Field2d;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class DriveTrain extends SubsystemBase {
 
-  private final Gyro m_gyro = new Gyro();
-  private final SwerveDriveKinematics m_kinematics = new SwerveDriveKinematics(kLocations);
-
-  private final Field2d m_field2d = new Field2d();
-  private final SwerveDriveOdometry m_odometry = 
-  new SwerveDriveOdometry(m_kinematics, m_gyro.getRotation2d(), SwerveModulePosition[] /*TODO input swerve module position*/,
-   new Pose2d(0, 0/*random values may change*/, new Rotation2d()));
-
       public DriveTrain() {
-        //reset the gyro because odometry start is 0
-        m_gyro.gyroCalibrate();
-        m_gyro.gyroReset();
+
       }
 
+  /**
+   * Example command factory method.
+   *
+   * @return a command
+   */
+  public Command exampleMethodCommand() {
+    // Inline construction of command goes here.
+    // Subsystem::RunOnce implicitly requires `this` subsystem.
+    return runOnce(
+        () -> {
+          /* one-time action goes here */
+        });
+  }
+
+  /**
+   * An example method querying a boolean state of the subsystem (for example, a digital sensor).
+   *
+   * @return value of some boolean subsystem state, such as a digital sensor.
+   */
   public boolean exampleCondition() {
     // Query some boolean state, such as a digital sensor.
     return false;
   }
 
   @Override
-  public void periodic(){
-    //updates the odometry positon
-    var m_odometryPose = m_odometry.update(m_gyro.getRotation2d(),
-     new SwerveModulePosition[] {
-      //TODO input swerve module
-     });
-
-     //Renews the field periodically
-    m_field2d.setRobotPose(m_odometry.getPoseMeters());
+  public void periodic() {
+    // This method will be called once per scheduler run
   }
 
   @Override
   public void simulationPeriodic() {
     // This method will be called once per scheduler run during simulation
-  }
-
-public Command resetOdometryBlueSide() {
-   return this.runOnce(
-        () ->
-            m_odometry.resetPosition(
-                m_gyro.getRotation2d(),
-                getModulePositions(),/*in need of module position*/
-                new Pose2d(2.1, 5, Rotation2d.fromDegrees(180))));
-}
-
-public Command resetOdometryRedSide() {
-   return this.runOnce(
-        () ->
-            m_odometry.resetPosition(
-                m_gyro.getRotation2d(),
-                getModulePositions(),/*in need of module position*/
-                new Pose2d(14.4, 5, Rotation2d.fromDegrees(180))));
-  }
-public void robotInit(){
-  SmartDashboard.putData("Field" ,m_field2d);
   }
 }
