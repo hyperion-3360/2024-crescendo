4ef4b74b5edce8e86dfa4cfdb0a4755404dfcb87
VictoriaDalton <victoria_m_dalton@hotmail.com>
2024-01-21 12:09:43 -0500

    climber elevator skel.

---
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -0,0 +1,18 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Climber extends SubsystemBase{
+    public void Climber() {}
+    
+    public void robotInit()
+    {
+
+    }
+    
+    @Override
+    public void periodic()
+    {
+
+    }
+}
---
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -0,0 +1,18 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Elevator extends SubsystemBase{
+    public void Elevator() {}
+    
+    public void robotInit()
+    {
+
+    }
+    
+    @Override
+    public void periodic()
+    {
+
+    }
+}
