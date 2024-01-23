bb7c826045b688257030011352a65426c367eea0
Igor-Volkov3360 <igorvolkovtravail@gmail.com>
2024-01-22 19:12:41 -0500

    quick tired skeleton

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -1,32 +1,62 @@
 package frc.robot.subsystems;
 
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
+enum elevatorLevel {
+HIGH,
+LOW,
+INTAKE
+};
+
 public class Elevator extends SubsystemBase{
+
+    DigitalInput topLimitSwitch = new DigitalInput(0);
+    DigitalInput intakeLimitSwitch = new DigitalInput(1);
+    DigitalInput lowLimitSwitch = new DigitalInput(2);
     
-    private TalonSRX m_elevatorRight = new TalonSRX(Constants.SubsystemConstants.kelevatorRightId); 
-    private TalonSRX m_elevatorLeft = new TalonSRX(Constants.SubsystemConstants.kelevatorLeftId); 
+    private Spark m_elevatorRight = new Spark(Constants.SubsystemConstants.kelevatorRightId); 
+    private Spark m_elevatorLeft = new Spark(Constants.SubsystemConstants.kelevatorLeftId); 
     
     // same as climber.java
     public void Elevator() {
         
         m_elevatorLeft.setInverted(true);
-        m_elevatorLeft.configFactoryDefault();
-        m_elevatorRight.configFactoryDefault();
-        m_elevatorLeft.follow(m_elevatorRight);
+        m_elevatorLeft.addFollower(m_elevatorRight);
     }
     
     public void robotInit()
     {
-
+    Shuffleboard.getTab("Spark max").add("spark channel", m_elevatorLeft.getChannel());
     }
     
     @Override
     public void periodic()
     {
 
+    }
+
+    public void setElevator(boolean high, boolean low, boolean ) 
+    {
+        switch () {
+            case 1:
+            while (lowLimitSwitch.isAnalogTrigger() == false &&) {
+                 m_elevatorLeft.set(0.1);
+            }
+              break;
+            case 2:
+              while (topLimitSwitch.isAnalogTrigger() == false ) {
+                 m_elevatorLeft.set(0.1);
+            }
+              break;
+            case 3:
+               while (intakeLimitSwitch.isAnalogTrigger() == false) {
+                 m_elevatorLeft.set(0.1);
+            }
+              break;
+          }
     }
 }
