bdeee29475f2b02f6608a0b444ff7ff708bde176
Igor-Volkov3360 <igorvolkovtravail@gmail.com>
2024-01-23 13:03:01 -0500

    addding commands

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -1,14 +1,15 @@
 
 package frc.robot.subsystems;
 
-import com.ctre.phoenix.motorcontrol.MotorCommutation;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import frc.robot.Constants.ElevatorConstants;
 
 enum elevatorLevel {
 HIGH,
@@ -17,11 +18,6 @@ INTAKE
 };
 
 public class Elevator extends SubsystemBase{
-
-  //making the limit switches
-    DigitalInput topLimitSwitch = new DigitalInput(0);
-    DigitalInput intakeLimitSwitch = new DigitalInput(1);
-    DigitalInput lowLimitSwitch = new DigitalInput(2);
     
     //instancing the motor controllers
     private CANSparkMax m_elevatorRight = new CANSparkMax(Constants.SubsystemConstants.kelevatorRightId, MotorType.kBrushless); 
@@ -37,6 +33,7 @@ public class Elevator extends SubsystemBase{
         m_elevatorRight.restoreFactoryDefaults();
         m_elevatorLeft.setInverted(true);
         m_elevatorLeft.follow(m_elevatorRight);
+        m_elevatorTarget  =; // TODO set the elevator target to an encoder position
     }
     
     public void robotInit()
@@ -51,18 +48,34 @@ public class Elevator extends SubsystemBase{
     }
 
 public void setElevator(elevatorLevel m_elevatorLevel) {
-      {
+      
             switch (m_elevatorLevel) {
               case HIGH:
-              
+              m_elevatorTarget = ElevatorConstants.highTarget;
                 break;
               case LOW:
-              
+              m_elevatorTarget = ElevatorConstants.lowTarget;
                 break;
               case INTAKE:
-               
+               m_elevatorTarget = ElevatorConstants.intakeTarget;
                 break;
+                default: m_elevatorTarget = ElevatorConstants.intakeTarget;
             }
+          }
+
+         public void setElevatorSpeed(double elevatorSpeed) {
+      m_elevatorLeft.set(elevatorSpeed);
+      m_elevatorRight.set(elevatorSpeed);
     }
+
+public Command stop() {
+  return this.runOnce(
+  () -> {
+  m_elevatorRight.stopMotor();
+    
+    });
+    }
+    
+
   }
-}
+
