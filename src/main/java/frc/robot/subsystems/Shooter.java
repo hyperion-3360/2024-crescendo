72f63d4b4a928fda97da67f6d7061b1e0700ad40
Buddy002 <louisrigu2gmail.com>
2024-01-24 17:54:53 -0500

    Stop command for the shooter

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,14 +1,16 @@
 package frc.robot.subsystems;
 
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import java.util.logging.Level;
+
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.CAN;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
@@ -21,47 +23,90 @@ public class Shooter extends SubsystemBase {
 
     // Create new motors
 
-    private CANSparkMax m_RightMaster = new CANSparkMax(Constants.SubsystemConstants.kShooterRightMasterId, MotorType.kBrushless);
-    private CANSparkMax m_RightFollower = new CANSparkMax(Constants.SubsystemConstants.kShooterRightFollowerId, MotorType.kBrushless);
-    private CANSparkMax m_LeftMaster = new CANSparkMax(Constants.SubsystemConstants.kShooterLeftMasterId, MotorType.kBrushless);
-    private CANSparkMax m_LeftFollower = new CANSparkMax(Constants.SubsystemConstants.kShooterLeftFollowerId, MotorType.kBrushless);
+    private static final double stopMotorShooter = 0;
+    private CANSparkMax m_rightMaster = new CANSparkMax(Constants.SubsystemConstants.kShooterRightMasterId, MotorType.kBrushless);
+    private CANSparkMax m_rightFollower = new CANSparkMax(Constants.SubsystemConstants.kShooterRightFollowerId, MotorType.kBrushless);
+    private CANSparkMax m_leftMaster = new CANSparkMax(Constants.SubsystemConstants.kShooterLeftMasterId, MotorType.kBrushless);
+    private CANSparkMax m_leftFollower = new CANSparkMax(Constants.SubsystemConstants.kShooterLeftFollowerId, MotorType.kBrushless);
     
     public void Shooter() {
 
-    // Config motors
+        // Config motors
+        m_leftMaster.setInverted(true);
+        m_leftFollower.setInverted(true);
 
-    m_LeftMaster.setInverted(true);
-    m_LeftFollower.setInverted(true);
+        m_rightMaster.restoreFactoryDefaults();
+        m_rightFollower.restoreFactoryDefaults();
+        m_leftMaster.restoreFactoryDefaults();
+        m_leftFollower.restoreFactoryDefaults();
 
-    m_RightMaster.restoreFactoryDefaults();
-     m_RightFollower.restoreFactoryDefaults();
-     m_LeftMaster.restoreFactoryDefaults();
-     m_LeftFollower.restoreFactoryDefaults();
+        m_leftFollower.follow(m_leftMaster);
+        m_rightFollower.follow(m_rightMaster);
+        
+    }
 
-    m_LeftFollower.follow(m_LeftMaster);
-    m_RightFollower.follow(m_RightMaster);
+    public void robotInit(){
 
- 
 
     }
-public void robotInit(){
+
+    @Override
+    public void periodic() {
+            
+    }
+
+    public void setSpeed(double rightMasterSpeed, double leftMasterSpeed) {
+        m_rightMaster.set(rightMasterSpeed);
+        m_leftMaster.set(leftMasterSpeed);
+
+    }
+
+    public void highGoal(double setDistance) {
 
 
-}
+    }
 
-@Override
-public void periodic() {
+    public Command Stop() {
         
+        if() {
+
+         m_leftMaster.set(stopMotorShooter);
+         m_rightMaster.set(stopMotorShooter);
+
+
+    } else {
+
+        return null;
+
+
+    
+    }
+    }
+
+
+    
+    public void level(shoot) {
+        
+
+    switch(shoot) {
+
+    case LOW:
+
+    m_leftMaster.set(0.3);
+    m_rightMaster.set(0.3);
+
+    break;
+
+    case HIGH:
+
+    m_leftMaster.set(0.8);
+    m_rightMaster.set(0.8);
+
+
+    }
+
+
+    }
+
 }
 
-public void setSpeed(double rightMasterSpeed, double leftMasterSpeed) {
-
-    m_RightMaster.set(rightMasterSpeed);
-    m_LeftMaster.set(leftMasterSpeed);
-
-}
-public void highGoal(double setDistance) {
-
-
-}
-}
