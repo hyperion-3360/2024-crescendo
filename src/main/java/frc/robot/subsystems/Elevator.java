1872f5fa6bd440c9f8a9aa366a75fc1213a072f4
Igor-Volkov3360 <igorvolkovtravail@gmail.com>
2024-01-24 17:02:21 -0500

    neo works!!!

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -19,23 +19,21 @@ INTAKE
 
 public class Elevator extends SubsystemBase{
     
-  //DigitalInput bottomlimitSwitch = new DigitalInput(1);
+  DigitalInput bottomlimitSwitch = new DigitalInput(1);
 
     //instancing the motor controllers
-    //private CANSparkMax m_elevatorRight = new CANSparkMax(Constants.SubsystemConstants.kelevatorRightId, MotorType.kBrushless); 
+    private CANSparkMax m_elevatorRight = new CANSparkMax(Constants.SubsystemConstants.kelevatorRightId, MotorType.kBrushless); 
     private CANSparkMax m_elevatorLeft = new CANSparkMax(Constants.SubsystemConstants.kelevatorLeftId, MotorType.kBrushless); 
     
-    //private double m_elevatorTarget = ElevatorConstants.kIntakeTarget;
+    private double m_elevatorTarget = ElevatorConstants.kIntakeTarget;
 
     //creating an elevator
     public Elevator() {
         //configures the CANSparkMax controllers
          m_elevatorLeft.restoreFactoryDefaults();
-       //m_elevatorRight.restoreFactoryDefaults();
+       m_elevatorRight.restoreFactoryDefaults();
         m_elevatorLeft.setInverted(true);
-        //m_elevatorLeft.follow(m_elevatorRight);
-        //m_elevatorTarget = ; // TODO set the elevator target to an meter unit
-        setElevatorSpeedmmmm(0.1);
+        m_elevatorLeft.follow(m_elevatorRight);
     }
     
     public void robotInit()
@@ -50,53 +48,53 @@ public class Elevator extends SubsystemBase{
 
     }
 
-// public void setElevator(elevatorLevel m_elevatorLevel) {
+public void setElevator(elevatorLevel m_elevatorLevel) {
       
-//             switch (m_elevatorLevel) {
-//               case HIGH:
-//               this.m_elevatorTarget = ElevatorConstants.kHighTarget + 
-//               setelevatorAngleFineTuning(0.1);
-//                 break;
-//               case LOW:
-//               this.m_elevatorTarget = ElevatorConstants.kLowTarget + 
-//               setelevatorAngleFineTuning(0.1);
-//                 break;
-//               case INTAKE:
-//                this.m_elevatorTarget = ElevatorConstants.kIntakeTarget;
-//                 break;
-//                 default: this.m_elevatorTarget = ElevatorConstants.kIntakeTarget;
-//             }
-//           }
+            switch (m_elevatorLevel) {
+              case HIGH:
+              this.m_elevatorTarget = ElevatorConstants.kHighTarget + 
+              setelevatorAngleFineTuning(0.1);
+                break;
+              case LOW:
+              this.m_elevatorTarget = ElevatorConstants.kLowTarget + 
+              setelevatorAngleFineTuning(0.1);
+                break;
+              case INTAKE:
+               this.m_elevatorTarget = ElevatorConstants.kIntakeTarget;
+                break;
+                default: this.m_elevatorTarget = ElevatorConstants.kIntakeTarget;
+            }
+          }
 
-// public void setElevatorSpeed(double m_elevatorSpeed) {
-//       m_elevatorLeft.set(m_elevatorSpeed);
-//       m_elevatorRight.set(m_elevatorSpeed);
-//     }
+public void setElevatorSpeed(double m_elevatorSpeed) {
+      m_elevatorLeft.set(m_elevatorSpeed);
+      m_elevatorRight.set(m_elevatorSpeed);
+    }
 
     public void setElevatorSpeedmmmm(double m_elevatorSpeedmmm) {
       m_elevatorLeft.set(m_elevatorSpeedmmm);
       };
     
 
-// public Command stop() {
-//   return this.runOnce(
-//   () -> {
-//   m_elevatorLeft.stopMotor();
-//   m_elevatorRight.stopMotor();
-//     });
-//     }
+public Command stop() {
+  return this.runOnce(
+  () -> {
+  m_elevatorLeft.stopMotor();
+  m_elevatorRight.stopMotor();
+    });
+    }
     
-// public void isAtBottom() {
-//   if(bottomlimitSwitch.get()) {
-//   m_elevatorLeft.restoreFactoryDefaults();
-//   m_elevatorRight.restoreFactoryDefaults();
-//   }
-//   }
+public void isAtBottom() {
+  if(bottomlimitSwitch.get()) {
+  m_elevatorLeft.restoreFactoryDefaults();
+  m_elevatorRight.restoreFactoryDefaults();
+    }
+  }
 
-// public double setelevatorAngleFineTuning(double m_elevatorAngle) {
-//   //TODO add aprilTag math when aprilTag done
-//   return m_elevatorAngle ;
-// }
+public double setelevatorAngleFineTuning(double m_elevatorAngle) {
+  //TODO add aprilTag math when aprilTag done
+  return m_elevatorAngle ;
+}
 
 }
 
