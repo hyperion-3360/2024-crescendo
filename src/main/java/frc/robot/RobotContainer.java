ca360c67bd23530de4476235560d3ccbdd9f60f0
Igor-Volkov3360 <igorvolkovtravail@gmail.com>
2024-01-23 19:50:05 -0500

    commit for debug

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,7 +9,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Constants.OperatorConstants;
-import frc.robot.subsystems.DriveTrain;
+//import frc.robot.subsystems.DriveTrain;
 import frc.robot.subsystems.Elevator;
 
 /**
@@ -21,26 +21,29 @@ import frc.robot.subsystems.Elevator;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
 
-  private final DriveTrain m_drive = new DriveTrain();
-
+  //private final DriveTrain m_drive = new DriveTrain();
+  private final Elevator elvt = new Elevator();
   // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+  //private final CommandXboxController m_driverController =
+      // new CommandXboxController(OperatorConstants.kDriverControllerPort);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
-    m_drive.setDefaultCommand(
-        m_drive.driveCommand(
-            () -> {
-              final var driverY = m_driverController.getLeftY();
-              return driverY;
-            },
-            () -> {
-              final var driverX = m_driverController.getLeftX();
-              return driverX;
-            },
-            () -> -m_driverController.getRightX(),
-            false));
+
+    System.out.print("CONTA>INER");
+
+    // m_drive.setDefaultCommand(
+    //     m_drive.driveCommand(
+    //         () -> {
+    //           final var driverY = m_driverController.getLeftY();
+    //           return driverY;
+    //         },
+    //         () -> {
+    //           final var driverX = m_driverController.getLeftX();
+    //           return driverX;
+    //         },
+    //         () -> -m_driverController.getRightX(),
+    //         false));
     // Configure the trigger bindings
     configureBindings();
   }
--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -4,6 +4,7 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
+import edu.wpi.first.util.sendable.Sendable;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -20,25 +21,32 @@ INTAKE
 
 public class Elevator extends SubsystemBase{
     
+  DigitalInput bottomlimitSwitch = new DigitalInput(1);
+
     //instancing the motor controllers
-    private CANSparkMax m_elevatorRight = new CANSparkMax(Constants.SubsystemConstants.kelevatorRightId, MotorType.kBrushless); 
+    //private CANSparkMax m_elevatorRight = new CANSparkMax(Constants.SubsystemConstants.kelevatorRightId, MotorType.kBrushless); 
     private CANSparkMax m_elevatorLeft = new CANSparkMax(Constants.SubsystemConstants.kelevatorLeftId, MotorType.kBrushless); 
     
     private double m_elevatorTarget;
 
     //creating an elevator
-    public void Elevator() {
-        
+    public Elevator() {
+        System.out.print("ELEVATOORit");
         //configures the CANSparkMax controllers
-        m_elevatorLeft.restoreFactoryDefaults();
-        m_elevatorRight.restoreFactoryDefaults();
+        // m_elevatorLeft.restoreFactoryDefaults();
+       //m_elevatorRight.restoreFactoryDefaults();
         m_elevatorLeft.setInverted(true);
-        m_elevatorLeft.follow(m_elevatorRight);
+       // m_elevatorLeft.follow(m_elevatorRight);
         //m_elevatorTarget = ; // TODO set the elevator target to an meter unit
     }
     
     public void robotInit()
     {
+      System.out.print("robot niit");
+      // SmartDashboard.putData("motor test", setElevatorSpeedmmmm(0.1));
+      // setElevatorSpeedmmmm(0.1);
+      m_elevatorLeft.set(0.1);
+      System.out.print("it works");
     }
     
     @Override
@@ -65,17 +73,29 @@ public void setElevator(elevatorLevel m_elevatorLevel) {
 
 public void setElevatorSpeed(double m_elevatorSpeed) {
       m_elevatorLeft.set(m_elevatorSpeed);
-      m_elevatorRight.set(m_elevatorSpeed);
+      //m_elevatorRight.set(m_elevatorSpeed);
     }
 
-public Command stop() {
-  return this.runOnce(
-  () -> {
-  m_elevatorRight.stopMotor();
+    public Sendable setElevatorSpeedmmmm(double m_elevatorSpeedmmm) {
+      m_elevatorLeft.set(m_elevatorSpeedmmm);
+      return setElevatorSpeedmmmm(m_elevatorSpeedmmm);
+        
+      };
     
-    });
-    }
+
+//public Command stop() {
+  //return this.runOnce(
+  //() -> {
+  //m_elevatorRight.stopMotor();
     
+    //});
+    //}
+    
+public void isAtBottom() {
+  if(!bottomlimitSwitch.equals(null));
+  m_elevatorLeft.restoreFactoryDefaults();
+  //m_elevatorRight.restoreFactoryDefaults();
+}
 
   }
 
