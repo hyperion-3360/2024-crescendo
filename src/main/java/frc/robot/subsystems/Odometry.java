95352d61760282150656bfdedd14d989e2e763a8
Igor-Volkov3360 <igorvolkovtravail@gmail.com>
2024-01-31 10:38:56 -0500

    removed drivetrain

--- src/main/java/frc/robot/subsystems/Odometry.java
+++ src/main/java/frc/robot/subsystems/Odometry.java
@@ -1,9 +1,9 @@
 package frc.robot.subsystems;
 
 import static frc.robot.Constants.WCPSwerveModule.kLocations;
+
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
@@ -11,13 +11,10 @@ import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants;
-import frc.robot.math.Conversions;
 import frc.robot.subsystems.WCPSwerveModule.WCPSwerveModuleFactory;
 
-public class DriveTrain extends SubsystemBase {
-
-  private final SwerveModuleFactory m_moduleFactory = new WCPSwerveModuleFactory();
+public class Odometry extends SubsystemBase{
+     private final SwerveModuleFactory m_moduleFactory = new WCPSwerveModuleFactory();
   private final SwerveModule[] m_modules = m_moduleFactory.createModules();
 
   private SwerveModulePosition[] modulePositions() {
@@ -32,7 +29,7 @@ public class DriveTrain extends SubsystemBase {
   new SwerveDriveOdometry(m_kinematics, m_gyro.getRotation2d(), modulePositions()  /*TODO input swerve module position*/,
    new Pose2d(0, 0, new Rotation2d()));
 
-      public DriveTrain() {
+      public Odometry() {
         //reset the gyro because odometry start is 0
         m_gyro.gyroCalibrate();
         m_gyro.gyroReset();
@@ -47,9 +44,7 @@ public class DriveTrain extends SubsystemBase {
   public void periodic(){
     //updates the odometry positon
     var m_odometryPose = m_odometry.update(m_gyro.getRotation2d(),
-     new SwerveModulePosition[] {
-      //TODO input swerve module
-     });
+     modulePositions());
 
      //Renews the field periodically
     m_field2d.setRobotPose(m_odometry.getPoseMeters());
@@ -65,7 +60,7 @@ public Command resetOdometryBlueSide() {
         () ->
             m_odometry.resetPosition(
                 m_gyro.getRotation2d(),
-                modulePositions(),/*in need of module position*/
+                modulePositions(),
                 new Pose2d(2.1, 5, Rotation2d.fromDegrees(180))));
 }
 
@@ -74,7 +69,7 @@ public Command resetOdometryRedSide() {
         () ->
             m_odometry.resetPosition(
                 m_gyro.getRotation2d(),
-                modulePositions(),/*in need of module position*/
+                modulePositions(),
                 new Pose2d(14.4, 5, Rotation2d.fromDegrees(180))));
   }
 public void robotInit(){
