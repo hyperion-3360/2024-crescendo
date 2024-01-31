276728a360c1a1a7c516c77282905b946411ad7e
rose <rosemarie_dalton@hotmail.com>
2024-01-30 19:10:38 -0500

    copy

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,7 +1,7 @@
 package frc.robot.subsystems;
 
-import com.revrobotics.CANSparkLowLevel.MotorType;
-import com.revrobotics.CANSparkMax;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
@@ -14,26 +14,22 @@ public class Shooter extends SubsystemBase {
 
   // Create new motors
 
-  private CANSparkMax m_RightMaster =
-      new CANSparkMax(Constants.SubsystemConstants.kShooterRightMasterId, MotorType.kBrushless);
-  private CANSparkMax m_RightFollower =
-      new CANSparkMax(Constants.SubsystemConstants.kShooterRightFollowerId, MotorType.kBrushless);
-  private CANSparkMax m_LeftMaster =
-      new CANSparkMax(Constants.SubsystemConstants.kShooterLeftMasterId, MotorType.kBrushless);
-  private CANSparkMax m_LeftFollower =
-      new CANSparkMax(Constants.SubsystemConstants.kShooterLeftFollowerId, MotorType.kBrushless);
+  private TalonSRX m_RightMaster = new TalonSRX(Constants.ShooterConstants.kRightMasterid);
+  private TalonSRX m_RightFollower = new TalonSRX(Constants.ShooterConstants.kRightFollowerid);
+  private TalonSRX m_LeftMaster = new TalonSRX(Constants.ShooterConstants.kLeftMasterid);
+  private TalonSRX m_LeftFollower = new TalonSRX(Constants.ShooterConstants.kLeftFollowerid);
 
-  public Shooter() {
+  public void Shooter() {
 
     // Config motors
 
     m_LeftMaster.setInverted(true);
     m_LeftFollower.setInverted(true);
 
-    // m_RightMaster.configFactoryDefault();
-    // m_RightFollower.configFactoryDefault();
-    // m_LeftMaster.configFactoryDefault();
-    // m_LeftFollower.configFactoryDefault();
+    m_RightMaster.configFactoryDefault();
+    m_RightFollower.configFactoryDefault();
+    m_LeftMaster.configFactoryDefault();
+    m_LeftFollower.configFactoryDefault();
 
     m_LeftFollower.follow(m_LeftMaster);
     m_RightFollower.follow(m_RightMaster);
@@ -46,9 +42,8 @@ public class Shooter extends SubsystemBase {
 
   public void setSpeed(double rightMasterSpeed, double leftMasterSpeed) {
 
-    // m_RightMaster.set(ControlMode.Velocity, rightMasterSpeed);
-    // m_LeftMaster.set(ControlMode.Velocity, leftMasterSpeed);
-
+    m_RightMaster.set(ControlMode.Velocity, rightMasterSpeed);
+    m_LeftMaster.set(ControlMode.Velocity, leftMasterSpeed);
   }
 
   public void highGoal(double setDistance) {}
