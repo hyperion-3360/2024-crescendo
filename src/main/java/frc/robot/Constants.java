d35b8fd9bdb14372d176c0f39944a89d45597235
shdalton <stephane.h.dalton@gmail.com>
2024-02-02 15:28:15 -0500

    remove build folder and WCPSwerve constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -193,41 +193,4 @@ public final class Constants {
     public static final double kangleWristscoreNote = 155.0;
     public static final double kangleFingerscoreNote = 155.0;
   }
-  ;
-
-  public static class WCPSwerveModule {
-
-    // public static final WCPSwerveModuleConfig[] kConfigs = {
-    //   new WCPSwerveModuleConfig(2, 1, 0, 9650),
-    //   new WCPSwerveModuleConfig(4, 3, 1, 20756.0),
-    //   new WCPSwerveModuleConfig(6, 5, 2, 1072.0),
-    //   new WCPSwerveModuleConfig(8, 7, 3, 13555.0)
-    // };
-    public static final Translation2d[] kLocations = {
-      // these values will have to be verified/edited
-      new Translation2d(0.3525, 0.275),
-      new Translation2d(0.3525, -0.275),
-      new Translation2d(-0.3525, -0.275),
-      new Translation2d(-0.3525, 0.275)
-    };
-
-    public static final double kAnalogToDeg = 360.0 / 28000;
-    public static final double kDegToAnalog = 1.0 / kAnalogToDeg;
-
-    public static final double kTickToMeter = 2.1 / 102260;
-    public static final double kTickToMeterPerS = 10.0 * kTickToMeter;
-
-    public static final double kMeterPerSToTick = 1.0 / kTickToMeterPerS;
-
-    public static final double kTurnKp = 0.07; // was .15
-    public static final double kTurnKi = 0.0;
-    public static final double kTurnKd = 0.05;
-    public static final double kTurnIZone = 0.0;
-
-    public static final double kDriveKp = 0.02;
-    public static final double kDriveKi = 0.0;
-    public static final double kDriveKd = 0.08;
-    public static final double kDriveKf = 0.04625;
-    public static final double kDriveIZone = 0.0;
-  }
 }
