7de47c4a76dea2bf69bdea91e5c85f1cf1574e23
Igor-Volkov3360 <igorvolkovtravail@gmail.com>
2024-02-04 22:06:16 -0500

    imported Rosemarie's shooter

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,15 +1,13 @@
 package frc.robot.subsystems;
 
-// import edu.wpi.first.wpilibj.DigitalInput;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkBase.IdleMode;
 
-import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.Constants;
-import frc.robot.math.CurveFunction;
 
 public class Shooter extends SubsystemBase {
   public enum shootSpeed {
@@ -20,51 +18,59 @@ public class Shooter extends SubsystemBase {
     STOP
   }
 
-  private static double highSpeed = 0.5;
-  private static double lowSpeed = 0.4;
-  private static double intakeSpeed = 0.4;
+  private static double highSpeed = 0.5; // 55% seem good
+  private static double lowSpeed = 0.04;
+  private static double intakeSpeed = 0.3;
   private static double trapSpeed = 0;
   private static double stopSpeed = 0;
+  private static double rampRate = 5;
 
   private double m_speed = 0;
 
-  private double m_shooterTargetSpeed;
+  private CANSparkMax m_leftMaster = new CANSparkMax(Constants.SubsystemConstants.kLeftMasterId, MotorType.kBrushless);
+  private CANSparkMax m_rightMaster = new CANSparkMax(Constants.SubsystemConstants.kRightMasterId, MotorType.kBrushless);
+  private CANSparkMax m_leftFollower = new CANSparkMax(Constants.SubsystemConstants.kLeftFollowerId, MotorType.kBrushless);
+  private CANSparkMax m_rightFollower = new CANSparkMax(Constants.SubsystemConstants.kRightFollowerId, MotorType.kBrushless);
 
-  private CANSparkMax m_leftMaster = new CANSparkMax(
-    Constants.SubsystemConstants.kLeftMasterId, MotorType.kBrushless);
-  private CANSparkMax m_rightMaster = new CANSparkMax(
-    Constants.SubsystemConstants.kRightMasterId, MotorType.kBrushless);
-  private CANSparkMax m_leftFollower = new CANSparkMax(
-    Constants.SubsystemConstants.kLeftFollowerId, MotorType.kBrushless);
-  private CANSparkMax m_rightFollower = new CANSparkMax(
-    Constants.SubsystemConstants.kLeftFollowerId, MotorType.kBrushless);
-
-    private CurveFunction m_curve = new CurveFunction();
 
   public Shooter() {
 
-    m_rightMaster.restoreFactoryDefaults();
-    m_rightMaster.setInverted(false);
-    m_rightMaster.burnFlash();
-
     m_leftMaster.restoreFactoryDefaults();
+    m_rightMaster.restoreFactoryDefaults();
+    m_leftFollower.restoreFactoryDefaults();
+    m_rightFollower.restoreFactoryDefaults();
+
     m_leftMaster.setInverted(true);
+    m_leftFollower.setInverted(true);
+
+    m_leftMaster.setIdleMode(IdleMode.kCoast);
+    m_leftFollower.setIdleMode(IdleMode.kCoast);
+    m_rightMaster.setIdleMode(IdleMode.kCoast);
+    m_rightFollower.setIdleMode(IdleMode.kCoast);
+
+    m_leftFollower.follow(m_leftMaster);
+    m_rightFollower.follow(m_rightMaster);
+
+    m_rightMaster.setOpenLoopRampRate(rampRate);
+    m_leftMaster.setOpenLoopRampRate(rampRate);
+    m_rightFollower.setOpenLoopRampRate(rampRate);
+    m_leftFollower.setOpenLoopRampRate(rampRate);
+
+    m_rightMaster.burnFlash();
+    m_leftFollower.burnFlash();
+    m_rightFollower.burnFlash();
     m_leftMaster.burnFlash();
 
-    m_leftFollower.follow(m_leftMaster, true);
-    m_rightFollower.follow(m_rightMaster, false);
+    
   }
 
   @Override
   public void periodic() {
-    if (m_speed == 0.0) {
-      m_curve.stop();
-    } else {
-      Double adjustedSpeed = m_curve.adjustPeriodic();
-      if (adjustedSpeed != null) {
-        m_rightMaster.set(adjustedSpeed);
-      }
-    }
+  // calcSpeed();
+  m_leftMaster.set(m_speed);
+  m_rightMaster.set(m_speed);
+        
+  // System.out.println("speed " + m_leftMaster.getAppliedOutput());
   }
 
   private void setShootingLevel(shootSpeed shoot) {
@@ -72,44 +78,33 @@ public class Shooter extends SubsystemBase {
     switch (shoot) {
       case LOW:
         m_speed = lowSpeed;
-        m_shooterTargetSpeed = lowSpeed;
         break;
 
       case HIGH:
         m_speed = highSpeed;
-        m_shooterTargetSpeed = highSpeed; 
         break;
 
       case STOP:
         m_speed = stopSpeed;
-        m_shooterTargetSpeed = stopSpeed;
         break;
 
       case TRAP:
         m_speed = trapSpeed;
-        m_shooterTargetSpeed = trapSpeed;
         break;
 
       case INTAKE:
         m_speed = intakeSpeed;
-        m_shooterTargetSpeed = intakeSpeed;
         break;
     }
   }
 
-  public void setShooterSpeed() {
-    m_speed = m_curve.getMotorSpeed(highSpeed);
-    m_leftMaster.set(m_speed);
-    m_rightMaster.set(m_speed);
+  public Command shoot(shootSpeed shootSpeed) {
+    return this.runOnce(() -> setShootingLevel(shootSpeed))
+        .andThen(new WaitCommand(5).andThen(this.stop()));
   }
 
   public Command stop() {
-    return this.runOnce(
-      () -> setShootingLevel(shootSpeed.STOP)
-    );
+    return this.runOnce(() -> setShootingLevel(shootSpeed.STOP));
   }
-
-  public Command shoot(shootSpeed shootSpeed) {
-    return this.runOnce(() -> setShootingLevel(shootSpeed));
-  }
+  
 }
