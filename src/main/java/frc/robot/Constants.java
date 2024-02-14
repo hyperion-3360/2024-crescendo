eb39b7ba222b175d5e4451c3559dfa0da8642208
VictoriaDalton <victoria_m_dalton@hotmail.com>
2024-02-14 17:26:14 -0500

    just changed comments from not finished to good

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -213,13 +213,12 @@ public final class Constants {
     public static final double kangleShoulderscoreNote = 110.0; // SCORENOTE
     public static final double kangleElbowscoreNote = 40.0;
     public static final double kangleWristscoreNote = 155.0;
-    // not finished
+    // good
     public static final double kangleShoulderstoreNote = 1.0; // STORENOTE
     public static final double kangleElbowstoreNote = 120.0;
     public static final double kangleWriststoreNote = 155.0;
-    // not finish
-    public static final double kangleShoulderprepareToClimb =
-        110.0; // PREPARETOCLIMBgetAutonomousCommand()
+    // good
+    public static final double kangleShoulderprepareToClimb = 110.0; // PREPARETOCLIMB
     public static final double kangleElbowprepareToClimb = 60.0;
     public static final double kangleWristprepareToClimb = 155.0;
   }
