9e1428b53687a5c51f8846f0d12f1cd2aa338755
Igor-Volkov3360 <igorVolkovTravail@gmail.com>
2024-02-18 19:40:24 -0500

    added quick fix for leds so they turn orange when vomitting

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -21,6 +21,7 @@ import frc.robot.subsystems.Climber.climberPos;
 import frc.robot.subsystems.Elevator;
 import frc.robot.subsystems.Elevator.elevatorHeight;
 import frc.robot.subsystems.LEDs;
+import frc.robot.subsystems.LEDs.State;
 import frc.robot.subsystems.Shooter;
 import frc.robot.subsystems.Trap;
 import frc.robot.subsystems.swerve.CTREConfigs;
@@ -172,9 +173,18 @@ public class RobotContainer {
         .onFalse(m_climber.climberGoToSelectedLevel(climberPos.STALL));
 
     m_driverController.a().toggleOnTrue(Sequences.intakeSequence(m_shooter, m_led));
-    m_driverController.b().toggleOnTrue(m_shooter.vomit());
-    m_driverController.y().toggleOnTrue(m_shooter.eject());
-    m_driverController.x().onTrue(m_elevator.extendTheElevator(elevatorHeight.INTAKE));
+    m_driverController
+        .b()
+        .toggleOnTrue(m_shooter.vomit().andThen(() -> m_led.setState(State.IDLE)));
+    m_driverController
+        .y()
+        .toggleOnTrue(m_shooter.eject().andThen(() -> m_led.setState(State.IDLE)));
+    m_driverController
+        .x()
+        .onTrue(
+            m_elevator
+                .extendTheElevator(elevatorHeight.INTAKE)
+                .andThen(() -> m_led.setState(State.IDLE)));
   }
 
   public Command getAutonomousCommand() {
