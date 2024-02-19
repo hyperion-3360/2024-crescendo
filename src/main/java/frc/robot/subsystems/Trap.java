2e0d76a3ff7fa431fea566fbe978686962ce2d6f
VictoriaDalton <victoria_m_dalton@hotmail.com>
2024-02-19 00:30:49 -0500

    small changes for test

--- src/main/java/frc/robot/subsystems/Trap.java
+++ src/main/java/frc/robot/subsystems/Trap.java
@@ -113,33 +113,33 @@ public class Trap extends SubsystemBase {
   public Command prepareToClimb() {
     return this.runOnce(
             () -> m_servoElbow.setAngle(Constants.TrapConstants.kangleElbowprepareToClimbdelayed1))
-        .andThen(new WaitCommand(0.3))
+        .andThen(new WaitCommand(0.2))
         .andThen(
             () ->
                 m_servoShoulder.setAngle(
                     Constants.TrapConstants.kangleShoulderprepareToClimbdelayed1))
-        .andThen(new WaitCommand(0.3))
+        .andThen(new WaitCommand(0.2))
         .andThen(
             () -> m_servoElbow.setAngle(Constants.TrapConstants.kangleElbowprepareToClimbdelayed2))
-        .andThen(new WaitCommand(0.3))
+        .andThen(new WaitCommand(0.2))
         .andThen(
             () ->
                 m_servoShoulder.setAngle(
                     Constants.TrapConstants.kangleShoulderprepareToClimbdelayed2))
-        .andThen(new WaitCommand(0.3))
+        .andThen(new WaitCommand(0.2))
         .andThen(
             () -> m_servoElbow.setAngle(Constants.TrapConstants.kangleElbowprepareToClimbdelayed3))
-        .andThen(new WaitCommand(0.3))
+        .andThen(new WaitCommand(0.2))
         .andThen(
             () ->
                 m_servoShoulder.setAngle(
                     Constants.TrapConstants.kangleShoulderprepareToClimbdelayed3))
-        .andThen(new WaitCommand(0.3))
+        .andThen(new WaitCommand(0.2))
         .andThen(() -> m_servoElbow.setAngle(Constants.TrapConstants.kangleElbowprepareToClimb))
-        .andThen(new WaitCommand(0.3))
+        .andThen(new WaitCommand(0.2))
         .andThen(
             () -> m_servoShoulder.setAngle(Constants.TrapConstants.kangleShoulderprepareToClimb))
-        .andThen(new WaitCommand(0.3))
+        .andThen(new WaitCommand(0.2))
         .andThen(() -> m_servoFinger.setAngle(Constants.TrapConstants.kfingerClosed))
         .andThen(() -> m_servoWrist.setAngle(Constants.TrapConstants.kangleWristprepareToClimb))
         .andThen(new WaitCommand(m_servoWrist.travelTime()));
