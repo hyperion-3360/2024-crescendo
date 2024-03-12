91399339e0e537feadde4d9255b8e22b01a3f0cf
Igor-Volkov3360 <igorVolkovTravail@gmail.com>
2024-03-12 18:30:55 -0400

    added a mini debug test unit to help debug the note targeter using field coordinates

--- src/main/java/frc/robot/commands/NoteLock.java
+++ src/main/java/frc/robot/commands/NoteLock.java
@@ -4,11 +4,17 @@ import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.trajectory.TrajectoryConfig;
+import edu.wpi.first.math.trajectory.TrajectoryGenerator;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants;
 import frc.robot.subsystems.Vision;
 import frc.robot.subsystems.swerve.Swerve;
+import java.util.List;
 import java.util.function.DoubleSupplier;
 
 public class NoteLock extends Command {
@@ -17,9 +23,11 @@ public class NoteLock extends Command {
   private DoubleSupplier m_translationSup;
   private DoubleSupplier m_strafeSup;
   private boolean m_lockedOnNote;
-  private Translation2d m_target;
+  private Translation2d m_target = null;
   private Vision m_vision;
+  // debug variables
   private boolean debugging;
+  private Field2d m_debugField2d;
 
   /**
    * constructor of the notelock class needs the subsystems and the axis required
@@ -39,20 +47,50 @@ public class NoteLock extends Command {
     this.m_vision = s_vision;
     this.m_translationSup = translationSup;
     this.m_strafeSup = strafeSup;
-    debugging = debug(true);
+
+    // debug calling
+    // debug();
+    debugging = false;
   }
 
   @Override
   public void execute() {
     // gives general robot positions and translation/strafe values
+    double neededAngle;
     var robotPosition = m_swerve.getPose().getTranslation();
     double translationVal =
         MathUtil.applyDeadband(m_translationSup.getAsDouble(), Constants.stickDeadband);
     double strafeVal = MathUtil.applyDeadband(m_strafeSup.getAsDouble(), Constants.stickDeadband);
     double rotationVal = m_swerve.getRotation2d().getRadians();
 
-    if (m_lockedOnNote) {
-      double neededAngle = getSinOfY(m_target.getX(), m_target.getY());
+    if (m_lockedOnNote == true) {
+
+      // #region "debug mini test unit"
+
+      if (debugging == true) {
+        debug();
+
+        neededAngle =
+            getDebugTanOfY(
+                m_target.getX(), m_target.getY(), robotPosition.getX(), robotPosition.getY());
+
+        var m_trajectory =
+            TrajectoryGenerator.generateTrajectory(
+                new Pose2d(robotPosition, Rotation2d.fromRadians(rotationVal)),
+                List.of(
+                    robotPosition.plus(new Translation2d(1, 1)),
+                    m_target.minus(new Translation2d(1, 1))),
+                new Pose2d(m_target, Rotation2d.fromRadians(rotationVal)),
+                new TrajectoryConfig(Units.feetToMeters(3.0), Units.feetToMeters(3.0)));
+        m_debugField2d.getObject("center point").setPose(new Pose2d(m_target, new Rotation2d(0)));
+        m_debugField2d.getObject("trajectory").setTrajectory(m_trajectory);
+
+        SmartDashboard.putData(m_debugField2d);
+        SmartDashboard.putNumber("needed angle", neededAngle);
+      }
+      // #endregion
+
+      neededAngle = getTanOfY(m_target.getX(), m_target.getY());
 
       // compute variables to feed the drive function of the swerves
       Translation2d pointToFace = m_target;
@@ -63,15 +101,17 @@ public class NoteLock extends Command {
       m_swerve.drive(
           new Translation2d(translationVal, strafeVal).times(Constants.Swerve.maxSpeed),
           (rotationVal + neededAngle) * Constants.Swerve.maxAngularVelocity,
+          // (rotationVal + neededAngle) * Constants.Swerve.maxAngularVelocity,
           false,
           true);
     } else {
       // checks if the detected note is stale
-      if (m_vision.isValidPos() && debugging == false) {
+      if (m_vision.isValidPos() == true && debugging == false) {
         m_target = m_vision.getNoteBoundingBox();
-        m_lockedOnNote = true;
+        // checks if the vision has a note targeted before setting m_locked on note to true
+        m_lockedOnNote = (m_lockedOnNote == false) ? (m_target != null) ? true : false : true;
       }
-      if (DriverStation.isAutonomousEnabled()) {
+      if (DriverStation.isAutonomous()) {
         if (m_vision.getVisibleNotes() == null) {
           m_swerve.drive(
               new Translation2d(translationVal, strafeVal)
@@ -85,16 +125,22 @@ public class NoteLock extends Command {
     }
   }
 
-  private double getSinOfY(double x, double y) {
+  private double getTanOfY(double x, double y) {
     /* AS is the adjacent side and OS is the opposite side */
     var AS = 1 - y;
     var OS = 0.5 - x;
     return Math.atan2(OS, AS);
   }
 
-  private boolean debug(boolean on) {
+  private double getDebugTanOfY(double x, double y, double robotX, double robotY) {
+    /* AS is the adjacent side and OS is the opposite side */
+    var AS = robotY - y;
+    var OS = robotX - x;
+    return Math.atan2(OS, AS);
+  }
+
+  private void debug() {
     m_lockedOnNote = true;
-    m_target = new Pose2d(0.735276, 0.556089, null).getTranslation();
-    return on;
+    m_target = new Translation2d(11, 12);
   }
 }
