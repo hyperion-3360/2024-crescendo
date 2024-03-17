31e25b9df90f1b489dee1f45da60454e18a1e7ce
Nikola Zelovic <nikola1998@hotmail.fr>
2024-03-17 01:03:09 -0400

    April tag locking for amp. TO BE TESTED

--- src/main/java/frc/robot/commands/AmpLock.java
+++ src/main/java/frc/robot/commands/AmpLock.java
@@ -2,10 +2,10 @@ package frc.robot.commands;
 
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants;
 import frc.robot.subsystems.Elevator;
@@ -13,16 +13,19 @@ import frc.robot.subsystems.LEDs;
 import frc.robot.subsystems.Vision;
 import frc.robot.subsystems.swerve.Swerve;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class AmpLock extends Command {
   private Swerve m_swerve;
   private Elevator m_elevator;
   private boolean m_isLocked;
   private Translation2d m_target;
+  // private double m_targetRotation;
   private LEDs m_led;
   private Vision m_vision;
   private AprilTagFieldLayout m_aprilTagFieldLayout;
   private Translation2d m_aprilTags[];
+  // private double m_aprilTagsRotation[];
   private int m_alliance_index;
 
   /**
@@ -49,13 +52,18 @@ public class AmpLock extends Command {
 
     if (m_aprilTagFieldLayout != null) {
       m_aprilTags = new Translation2d[Constants.VisionConstants.kAmpIndex.length];
+      // m_aprilTagsRotation = new double[Constants.VisionConstants.kAmpIndex.length];
 
       // initialize translation2d objects for each AprilTag on the field
       int i = 0;
       for (var tagId : Constants.VisionConstants.kAmpIndex) {
         var tagPose = m_aprilTagFieldLayout.getTagPose(tagId).get();
+        // var tagRot = tagPose.getRotation();
+
         // no need for Z
         m_aprilTags[i] = new Translation2d(tagPose.getX(), tagPose.getY());
+        // m_aprilTagsRotation[i] = tagRot.getZ();
+
         i++;
       }
 
@@ -83,25 +91,44 @@ public class AmpLock extends Command {
 
     if (m_aprilTagFieldLayout != null) {
 
-      double rotationVal = m_swerve.getRotation2d().getRadians();
+      // double rotationVal = m_swerve.getRotation2d().getRadians();
 
-      var cur_pos = m_swerve.getPose().getTranslation();
+      // var cur_pos = m_swerve.getPose().getTranslation();
 
-      if (m_isLocked) {
+      if (m_isLocked && m_vision.getPosition() != null) {
+        final var cameraPose2d = m_vision.getPosition();
+        var camTranslation = new Translation2d(cameraPose2d.getX(), cameraPose2d.getY());
+
         // compute rotation to apply to face the target
         Translation2d pointToFace = m_target;
-        Rotation2d rotationNeeded =
-            pointToFace.minus(m_swerve.getPose().getTranslation()).getAngle();
-        rotationVal = rotationNeeded.getRadians();
+        // double tagOrientation = m_targetRotation;
+
+        // distance from camera to tag -- get distance from robot
+        final var dx = camTranslation.getX() - pointToFace.getX();
+        final var dy = camTranslation.getX() - pointToFace.getX();
+        final var hyp = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
+        final var distanceFromTag = Math.sin(Constants.VisionConstants.kShooterCameraPitch) * hyp;
+
+        // Calulate the rotation needed to face the tag
+        // Correct for camera position. Anticlockwise rotatiozn
+        double camRot = cameraPose2d.getRotation().getRadians();
+        double rotationVal = camRot + Constants.VisionConstants.kShooterCameraPitch;
+
+        SmartDashboard.putNumber("Distance from target", distanceFromTag);
+        SmartDashboard.putNumber("Rotation needed (rad)", rotationVal);
 
         // compute elevator angle to shoot in target, assuming straightline not parabol
-        double distance2Target = cur_pos.getDistance(pointToFace);
+        double distance2Target = distanceFromTag; // cur_pos.getDistance(pointToFace);
         if (distance2Target <= Constants.VisionConstants.kAmpRiseElevatorDistance) {
           m_elevator.extendTheElevator(Elevator.elevatorHeight.LOW);
         }
         if (distance2Target <= Constants.VisionConstants.kAmpShootingDistance) {
           m_led.setState(LEDs.State.SHOOT_READY_SPEAKER);
         } else {
+          // Reset the odometry X,Y positioning before moving
+          // to make sure the robot knows where it is on then field !!
+          m_vision.resetOdometryTranslation2d(m_swerve);
+
           /* Drive */
           m_swerve.drive(
               pointToFace.times(Constants.Swerve.maxSpeed),
@@ -119,7 +146,11 @@ public class AmpLock extends Command {
             }
 
           if (selecteg_tag != -1) {
-            m_target = m_aprilTags[selecteg_tag];
+            // TODO: optimize this.. Meh recreating object each time...¯\_(ツ)_/¯
+            final var tagIdx =
+                Arrays.asList(Constants.VisionConstants.kAmpIndex).indexOf(selecteg_tag);
+            m_target = m_aprilTags[tagIdx];
+            // m_targetRotation = m_aprilTagsRotation[tagIdx];
             m_isLocked = true;
             m_led.setState(LEDs.State.PREPARE_SHOT_SPEAKER);
           } else m_led.setState(LEDs.State.IDLE);
--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -8,7 +8,9 @@ import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEvent;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.util.sendable.SendableBuilder;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.subsystems.swerve.Swerve;
 import java.util.Arrays;
 import java.util.EnumSet;
 import java.util.concurrent.atomic.AtomicReference;
@@ -21,6 +23,9 @@ public class Vision extends SubsystemBase {
   private final int kAprilTagRotStartIndex = 3;
   private final int kAprilTagAnglesStartIndex = 6;
   private final int kAprilTagIdsStartIndex = 9;
+
+  private final double kCoalescingTime = 0.25; // seconds
+  private double m_taglastUpdate = 0;
 
   final AtomicReference<double[]> m_tagValue = new AtomicReference<double[]>();
   final AtomicReference<double[]> m_detectionValue = new AtomicReference<double[]>();
@@ -90,24 +95,63 @@ public class Vision extends SubsystemBase {
       var numTags = info.length - kAprilTagIdsStartIndex;
       var angles = Arrays.copyOfRange(info, kAprilTagAnglesStartIndex, kAprilTagIdsStartIndex);
 
+      // Relative orientation between the camera and the april tag
+      // Rotation around the vertical axis is at index 1
+      // TODO: Jetson script returns an average value of these orientatione.
+      //  Not valid if more than 1 camera
       var tagRotation = Rotation2d.fromDegrees(angles[1]);
-      var robotRotation =  Rotation2d.fromDegrees(rotation[1]);;
 
-      m_currentPos = new Pose2d(new Translation2d(position[0], position[1]), robotRotation);
+      // TODO: rotation calculated by jetson script doesn't seem valid..
+      // Can't rely on it for now. using tagRotation instead. tbd
+      var robotRotation = Rotation2d.fromDegrees(rotation[2]);
+
+      m_currentPos = new Pose2d(new Translation2d(position[0], position[1]), tagRotation);
       m_visibleTags = new long[numTags];
       for (int idx = 0; idx < numTags; idx++)
         m_visibleTags[idx] = (long) info[idx + kAprilTagIdsStartIndex];
-    } else m_currentPos = null;
+
+      m_taglastUpdate = Timer.getFPGATimestamp();
+    } else if (Timer.getFPGATimestamp() - m_taglastUpdate > kCoalescingTime) {
+      m_currentPos = null;
+    }
 
     // check if we have a note detected using the AI system
     value = m_detectionValue.getAndSet(null);
     if (value != null) {
       m_detection = new Translation2d(value[0], value[1]);
-    } else m_detection = null;
+    } else {
+      m_detection = null;
+    }
   }
 
+  /**
+   * POSE2D OF THE CAMERA !!!!!!!!!!! Robot positionning needs to take into account the camera's
+   * pitch, yaw, roll and relative position in robot
+   *
+   * @return Camera position and rotation in field
+   */
   public Pose2d getPosition() {
     return m_currentPos;
+  }
+
+  /**
+   * Can be used to reset the robot's positioning in the field, (but not the heading ), based on the
+   * detected april tags TODO: Automatically reset the odometry instead of calling it manually, once
+   * the april tags values are more stable and robust...
+   *
+   * @param s_swerve Swerves subsystem
+   */
+  public void resetOdometryTranslation2d(Swerve s_swerve) {
+    if (isValidPos()) {
+      final var currentRobotPose = s_swerve.getPose().getTranslation();
+
+      final var tagPose = getPosition();
+      final var resetPose =
+          new Pose2d(
+              new Translation2d(tagPose.getX(), tagPose.getY()), currentRobotPose.getAngle());
+
+      s_swerve.setPose(resetPose);
+    }
   }
 
   public Translation2d getNoteCoord() {
@@ -130,12 +174,16 @@ public class Vision extends SubsystemBase {
   public void initSendable(SendableBuilder builder) {
     builder.setSmartDashboardType("Vision");
     builder.addBooleanProperty("Valid AprilTag", this::isValidPos, null);
-    builder.addDoubleProperty("Rotation", () -> getPosition().getRotation().getDegrees(), null);
-    builder.addDoubleProperty("Pose X", () -> getPosition().getX(), null);
-    builder.addDoubleProperty("Pose Y", () -> getPosition().getY(), null);
-    builder.addIntegerArrayProperty("Tag IDs", this::getVisibleTagIds, null);
-    builder.addBooleanProperty("Valid Note detection", this::isNoteDetected, null);
-    builder.addDoubleProperty("Note detection X", () -> getNoteCoord().getX(), null);
-    builder.addDoubleProperty("Note detection Y", () -> getNoteCoord().getY(), null);
+    if (m_currentPos != null) {
+      builder.addDoubleProperty("Rotation", () -> getPosition().getRotation().getDegrees(), null);
+      builder.addDoubleProperty("Pose X", () -> getPosition().getX(), null);
+      builder.addDoubleProperty("Pose Y", () -> getPosition().getY(), null);
+      builder.addIntegerArrayProperty("Tag IDs", this::getVisibleTagIds, null);
+    }
+    // builder.addBooleanProperty("Valid Note detection", this::isNoteDetected, null);
+    // if(getNoteCoord() != null){
+    //   builder.addDoubleProperty("Note detection X", () -> getNoteCoord().getX(), null);
+    //   builder.addDoubleProperty("Note detection Y", () -> getNoteCoord().getY(), null);
+    // }
   }
 }
