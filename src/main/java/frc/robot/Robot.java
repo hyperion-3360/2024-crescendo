f28923081b212b941fb422d0f1f6055e2811b6a3
shdalton <stephane.h.dalton@gmail.com>
2024-03-17 09:21:30 -0400

    enable live window in test mode

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -25,6 +25,8 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotInit() {
+    // since 2024 the Live Window is not enabled by default in test mode... hence this call
+    enableLiveWindowInTest(true);
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
